3e2987d15a052e77fcf990dc4d9f70689934f6ae
saurav <ssaurav0002@gmail.com>
2023-09-14 15:15:18 +0530

    initial commit

---
+++ ModuleFeedback/Controllers/EmployeesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ModuleFeedback.Data;
+using ModuleFeedback.Models;
+using ModuleFeedback.Models.Domain;
+
+namespace ModuleFeedback.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MVCDemoDbContext mvcDemoDbContext;
+
+        public EmployeesController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth
+            };
+
+            mvcDemoDbContext.Employees.Add(employee);
+            mvcDemoDbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees = await mvcDemoDbContext.Employees.ToListAsync();
+            return View(employees);
+        }
+
+
+
+    }
+}
---
+++ ModuleFeedback/Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModuleFeedback.Models.Domain;
+
+namespace ModuleFeedback.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+            { }
+
+        // employees will be the name of the table that will be created
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
